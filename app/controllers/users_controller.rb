baf744abcc80c2b5e7d9b3971a596ab99ac8a2c1
uavpliyc <uavpliyc@gmail.com>
2021-02-06 09:09:01 +0000

    [Add_book_user]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
       @user = current_user
     end
 
-    # def show
-    #   @user = User.find(params[:id])
-    # end
+    def show
+      @user = User.find(params[:id])
+    end
 
     def edit
       @user = User.find(params[:id])
+      redirect_to user_path(current_user) unless @user.id == current_user.id
     end
 
     def update
