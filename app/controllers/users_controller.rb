e8f3e56e0887e4ad874a33b1082376449071777c
uavpliyc <uavpliyc@gmail.com>
2021-02-06 02:37:42 +0000

    [Fix_login]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
       @user = current_user
     end
 
-    def show
-      @user = User.find(params[:id])
-    end
+    # def show
+    #   @user = User.find(params[:id])
+    # end
 
     def edit
       @user = User.find(params[:id])
