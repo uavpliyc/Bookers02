0fedcc12afff0a2a2b16298bc7644d4405a497c1
uavpliyc <uavpliyc@gmail.com>
2021-02-06 06:05:01 +0000

    [Fix_error]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
     users_path
   end
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
 
+  validates :title, presense: true
+  validates :body, presense: true, length:{ maximum: 200 }
   belongs_to :user
 
 end
